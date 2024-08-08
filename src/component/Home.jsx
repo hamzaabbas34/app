7624a2ec2516b48743ae75f77b17ac8f259ba790
hamzaabbas34 <abbasbahi289@gmail.com>
2024-08-08 19:27:33 +0500

    last

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
 			<div className="overlay"></div>
 			<Navbar />
 			<div className="w-100  h-[95vh] flex justify-center items-center px-10 lg:px-40 xl:px-40 2xl:px-40">
-				<div className="maindiv  grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 2xl:grid-cols-3">
+				<div className="maindiv  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3">
 					<div className="lf  2xl:col-span-2 xl:col-span-2 lg:col-span-2  flex justify-start items-center left">
 						<h2 className="text-4xl font-light leading-tight font-machin">
 							Hey I'm Hamza Abbas , a web developers focused on creating
